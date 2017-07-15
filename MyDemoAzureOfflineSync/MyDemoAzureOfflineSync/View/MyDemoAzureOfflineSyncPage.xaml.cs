4800cf8a7b95fbba95b6cc8d79cc52cc42c8b9ee
Henrique Nascimento <henriqu3@gmail.com>
2017-07-15 13:40:33 -0300

    initial import

---
+++ MyDemoAzureOfflineSync/MyDemoAzureOfflineSync/View/MyDemoAzureOfflineSyncPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Xamarin.Forms;
+
+namespace MyDemoAzureOfflineSync
+{
+    public partial class MyDemoAzureOfflineSyncPage : ContentPage
+    {
+        public MyDemoAzureOfflineSyncPage()
+        {
+            InitializeComponent();
+            BindingContext = new ContactsVM();
+        }
+    }
+}
